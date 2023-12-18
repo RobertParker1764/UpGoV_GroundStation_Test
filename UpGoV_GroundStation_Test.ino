e4d3d00f676733e6af7210a8c001d8a03029d9d0
Robert Parker <robertparker1764@gmail.com>
2023-12-18 14:43:29 -0800

    Version 1.0.3commit. Corrected LoRa radio sync at startup and added first test case.

--- UpGoV_GroundStation_Test.ino
+++ UpGoV_GroundStation_Test.ino
@@ -1,7 +1,7 @@
 // UpGoV Ground Station Test
-// Version: 1.02
+// Version: 1.0.3
 // Author: Jonah Parker
-// Date: 12/12/2023
+// Date: 12/14/2023
 
 
 /// ==============================  Include Files  ==================================
@@ -10,7 +10,7 @@
 #include <SPI.h>                      // Radio communications serial bus
 
 // ===============================  Constants  =====================================
-const char* VERSION = "1.02";
+const char* VERSION = "1.0.3";
 const uint8_t RADIO_SPI_CS = 8;
 const uint8_t RADIO_SPI_IRQ = 3;
 const double RF95_FREQ = 915.0;
@@ -25,7 +25,9 @@ RH_RF95 radioDriver = RH_RF95(RADIO_SPI_CS, RADIO_SPI_IRQ); // LoRa radio driver
 RHReliableDatagram radioManager(radioDriver, UPGOV_ADDR);   // LoRa radio manager object
 
 // Other global variables
-uint8_t buffer[MAX_MESSAGE_LENGTH]; // Message buffer
+uint8_t buf[MAX_MESSAGE_LENGTH]; // Message buffer
+char radioPacket[20];            // Buffer containing radio message
+int radioError = 0;              // The number of radio messages not receiving an ack
 
 void setup() {
     // Initialize the Serial Console interface (used for debugging only)
@@ -79,17 +81,16 @@ void setup() {
   uint8_t from;
   uint8_t to;
   while (!connected) {
-    messageLength = sizeof(buffer);
-    if (radioManager.recvfromAck(buffer, &messageLength, &from, &to)) {
-      // Message received. Is it to us?
-      Serial.println("Radio message received");
-      if (to == UPGOV_ADDR) {
-        // The message is to us. Is it from the ground station?
+    if (radioManager.available()) {
+      // Wait for a message addressed to us
+      uint8_t len = sizeof(buf);
+      uint8_t from;
+      if (radioManager.recvfromAck(buf, &len, &from)) {
+        // There is a message addressed to us. Check if it is from the ground station
         if (from == GROUND_STATION_ADDR) {
-          // Message is from the ground station. Is it a connect message?
-          buffer[messageLength] = 0;  // Terminate with a null character
-          if (!strncmp((char*)buffer, "connect", 7)) {
-            // Connect message received. Send connect message reply
+          // The message is from the Ground Station. Is it a "connect" message?
+          if (!strncmp((char*)buf, "connect", 7)) {
+            // It is a "connect" message. Send a "connect" reply.
             uint8_t data[] = "connect";
             if (radioManager.sendtoWait(data, sizeof(data), GROUND_STATION_ADDR)) {
               connected = true;
@@ -100,12 +101,41 @@ void setup() {
     }
   } // End radio connect loop
 
-  delay(500);
+  delay(2000);
 
+  Serial.println("Sending first message");
+  sendRadioMessage("MS:MSG 1", GROUND_STATION_ADDR);
+  delay(5000);
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+//=================== sendRadioMessage ======================
+// Sends a radio message to the specified address
+// Parameters:
+//  message:  The radio message that is to be sent
+//  address: The address the message is to be sent to
+// Return: True if an acknowledgment was received
+//=================================================================
+bool sendRadioMessage(const char * message, uint8_t address) {
+    strncpy(radioPacket, message, sizeof(radioPacket));
+  if (radioManager.sendtoWait((uint8_t *)radioPacket, sizeof(radioPacket), address)) {
+#ifdef DEBUG
+  Serial.print("Radio message sent: ");
+  Serial.println(message);
+#endif
+  return true;
+  } else {
+    radioError++;
+#ifdef DEBUG
+    Serial.println("No message ack");
+    Serial.print("Radio Error #: ");
+    Serial.println(radioError);
+#endif
+    return false;
+  }
 }
